7762f36ef0acd8ff1573c0ceefd2685f7472d20c
Kevin Shao <shaoqihen@gmail.com>
2026-07-23 23:21:22 -0400

    7_24f

--- copy_outputs.py
+++ copy_outputs.py
@@ -1,1 +1,13 @@
 scp -r "raven:/ptmp/kshao/jax/jaxmd-cli/equil/" "C:\Users\shaoq\Documents\Mainz\mlip\outputsfull\jaxequil"
+
+
+
+
+
+
+
+
+
+
+
+& "C:\Program Files\Git\usr\bin\scp.exe" -o "ProxyJump=kshao@gate1.mpcdf.mpg.de" "/c/Users/shaoq/Downloads/MACE-OFF24_medium.model" "kshao@dais11:/dais/fs/scratch/kshao/mlip/codes/7_23_convertingmaceoff/"
