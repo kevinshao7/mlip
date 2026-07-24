d637e04ab3fffc9432b2b0c50d31f53d782c8a61
Kevin Shao <shaoqihen@gmail.com>
2026-07-23 23:24:44 -0400

    7_24h

--- codes/7_23_convertingmaceoff/convert.py
+++ codes/7_23_convertingmaceoff/convert.py
@@ -1,9 +1,10 @@
 import test_torch_to_jax_water_npt as ttj
 import torch
+from pathlib import Path
 path = "MACE-OFF24_medium.model"
 torch_model = torch.load(
     path,
     map_location="cpu",
     weights_only=False,
 )
-ttj._save_mace_off_checkpoint(torch_model, "/jaxdir/")
+ttj._save_mace_off_checkpoint(torch_model, Path("jaxdir").resolve())
